8d024c4d445be4646f0a2838ff932c6445266910
pmacdona <pmacdona@noemail.net>
2020-07-12 20:57:51 +0000

    Test fixes.
    FossilOrigin-Name: b2fb7b4c47909c2d0f54af1f5f316faa2daec1e74af4d1e514b1e464510b6f67

--- src/jsiValue.c
+++ src/jsiValue.c
@@ -320,7 +320,7 @@ Jsi_Value *Jsi_ValueDupJSON(Jsi_Interp *interp, Jsi_Value *val)
     Jsi_DString pStr;
     Jsi_DSInit(&pStr);
     Jsi_ValueGetDString(interp, val, &pStr, JSI_OUTPUT_JSON);
-    Jsi_Value *valPtr = NULL;
+    Jsi_Value *valPtr = Jsi_ValueNew1(interp);
     if (Jsi_JSONParse(interp, Jsi_DSValue(&pStr), &valPtr, 0) != JSI_OK)
         Jsi_LogBug("bad json parse");
     Jsi_DSFree(&pStr);
